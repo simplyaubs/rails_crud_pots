71c2026219edac8ab06e2fea5437e07804a5ccd3
Aubrey Howell <simplyaubs@gmail.com>
2014-06-16 14:13:07 -0600

    User can update a pot from list.

--- app/controllers/pots_controller.rb
+++ app/controllers/pots_controller.rb
@@ -15,6 +15,21 @@ class PotsController < ApplicationController
     end
   end
 
+  def show
+    @pot = Pot.find(params[:id])
+  end
+
+  def edit
+    @pot = Pot.find(params[:id])
+  end
+
+  def update
+    @pot = Pot.find(params[:id])
+    @pot.update_attributes!(pot_params)
+
+    redirect_to pots_path
+  end
+
   private
   def pot_params
     params.require(:pot).permit(:size, :color)
--- spec/features/pots_spec.rb
+++ spec/features/pots_spec.rb
@@ -13,4 +13,25 @@ feature 'CRUD pots' do
     expect(page).to have_content 'Orange'
   end
 
+  scenario 'User can update a pot from list' do
+    visit '/'
+    expect(page).to have_content 'Welcome'
+    click_on 'Add a pot'
+    fill_in 'Size', with: 'Large'
+    fill_in 'Color', with: 'Orange'
+    click_on 'Add pot'
+    expect(page).to have_content 'Large'
+    expect(page).to have_content 'Orange'
+    click_on 'Large'
+    expect(page).to have_content 'Large'
+    expect(page).to have_content 'Orange'
+    click_on 'Edit'
+    fill_in 'Size', with: 'Small'
+    fill_in 'Color', with: 'Blue'
+    click_on 'Update pot'
+    expect(page).to have_content 'Small'
+    expect(page).to have_content 'Blue'
+    expect(page).to_not have_content 'Large'
+    expect(page).to_not have_content 'Orange'
+  end
 end
